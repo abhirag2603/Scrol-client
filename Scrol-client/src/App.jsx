b4dba8b1a8133fa5d3dd807d1b269c16acb7ed38
Abhirag <abhirag4169@gmail.com>
2024-08-04 00:20:53 +0530

    components initialization state initialization recoil

--- Scrol-client/src/App.jsx
+++ Scrol-client/src/App.jsx
@@ -1,2 +1,26 @@
-import React from "react";
+import React from 'react'
+import {BrowserRouter,Navigate,Routes,Route} from 'react-router-dom'
+import {RecoilRoot,} from 'recoil';
+import Login from './components/Login';
+import HomePage from './components/HomePage';
+import Profile from './components/Profile';
+import Register from './components/Register';
 
+const App = () => {
+  return (
+    <RecoilRoot>
+    <div className='app'>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/home' element={<HomePage/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/profile/:userId' element={<Profile/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+    </RecoilRoot>
+  )
+}
+
+export default App
